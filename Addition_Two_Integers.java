07516c24b00ed6fd8c06e00e54eda4e70b0da334
kunjanmulla10@gmail.com <kunjanmulla10@gmail.com>
2024-12-23 23:53:17 +0530

    Prog_3

---
+++ Addition_Two_Integers.java
@@ -0,0 +1,23 @@
+package SET_1;
+
+public class Addition_Two_Integers {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int a = 20 ;
+		int b = 90 ;
+		int c = a + b ;
+		int d = b - a ;
+		int e = a * b ;
+		double f =(double)( b / a );
+		float g = b % a ;      //Finding Reminder
+		System.out.println("Addition :"+c) ;
+		System.out.println("Substraction :"+d) ;
+		System.out.println("Multi :"+e) ;
+		System.out.println("Divide :"+f) ;
+		System.out.println("Reminder :"+g) ;
+		
+
+	}
+
+}
