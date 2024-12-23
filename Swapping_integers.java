15207ae3046dd2dc0c382674915df3c80fe1a8e1
kunjanmulla10@gmail.com <kunjanmulla10@gmail.com>
2024-12-23 23:56:19 +0530

    Prog_5

---
+++ Swapping_integers.java
@@ -0,0 +1,27 @@
+package SET_1;
+
+public class Swapping_integers {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int a = 10 ;
+		int b = 90 ;
+		System.out.println("Before Swap : a =" +a+ " b= "+b) ;
+		int temp = 0 ;
+		temp = a ;
+		a = b ;
+		b =temp ;
+		
+		System.out.println("After Swap : a ="+a+ " b= " +b) ;
+
+	}
+
+}
+/*
+ *     temp = 0
+ *     temp  =  10;  //
+ *     10 = 90 ;
+ *     90 = temp     //
+ *     
+ */
+*/
