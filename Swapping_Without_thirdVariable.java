ea5cb23e7c2108d4759f6cab4cc04cd47532233b
kunjanmulla10@gmail.com <kunjanmulla10@gmail.com>
2024-12-23 23:36:42 +0530

    pro_1

---
+++ Swapping_Without_thirdVariable.java
@@ -0,0 +1,17 @@
+package SET_1;
+
+public class Swapping_Without_thirdVariable {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int a = 10 ;
+		int b = 90 ;
+		System.out.println("Before Swap : a =" +a+ " b= "+b) ;
+		a = a + b ;
+		b = a- b;
+		a= a-b ;
+		System.out.println("After Swap : a =" +a+ " b= "+b) ;
+	
+	}
+
+}
