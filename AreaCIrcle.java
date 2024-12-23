028b44b1f5c20a0d1d19d3358dc967881aac9f86
kunjanmulla10@gmail.com <kunjanmulla10@gmail.com>
2024-12-23 23:49:12 +0530

    Prog_2

---
+++ AreaCIrcle.java
@@ -0,0 +1,12 @@
+package SET_1;
+
+public class AreaCIrcle {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+ 
+		 int r = 20 ;
+		 System.out.println("ARea of circle :"+(22/7)*r*r) ;
+	}
+
+}
