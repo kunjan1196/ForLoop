7f7eb7bec546d7a68fc7d17249f4cc80b44f7304
kunjanmulla10@gmail.com <kunjanmulla10@gmail.com>
2024-12-23 23:55:00 +0530

    Prog_4

---
+++ Area_Triangle.java
@@ -0,0 +1,16 @@
+package SET_1;
+
+public class Area_Triangle {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int base =  10;
+		int height = 7 ;
+		int area ;
+		area = (base * height);
+		System.out.println("Area of Triangle :"+area/2) ;
+		
+
+	}
+
+}
